7b2aae95d11afe5c837c02f1184b6584d8ba635c
Pierre Bédu <116664240+pierre-bedu-catie@users.noreply.github.com>
2024-05-21 11:58:18 +0200

    Update multimodal_dataloader_WIP.py

--- multimodal_dataloader_WIP.py
+++ multimodal_dataloader_WIP.py
@@ -10,13 +10,14 @@ from torchvision import utils
 import torchvision.transforms as transforms
 from datetime import datetime, timedelta
 from chronos import ChronosPipeline, ChronosConfig, ChronosTokenizer, MeanScaleUniformBins
+from torch.nn.utils.rnn import pad_sequence
 
 class DatasetMultimodal(Dataset):
     def __init__(self, df_pandas, absolute_start, absolute_end, transforms, images_tokenizer, pipeline_ts):
 
         #get a list of random start&end dates
         L=[]        
-        for i in range(1000):
+        for i in range(4):
             absolute_start = datetime.strptime(str(absolute_start), "%Y-%m-%d %H:%M:%S")
             absolute_end = datetime.strptime(str(absolute_end), "%Y-%m-%d %H:%M:%S")
             time_delta = absolute_end - absolute_start
@@ -38,7 +39,7 @@ class DatasetMultimodal(Dataset):
         return len(self.seq_dates)
     
     def __getitem__(self, idx):
-        
+        print("getitem is called")
         items_paths=[]
         while len(items_paths)==0: 
             date_start, date_end = self.seq_dates[idx]
@@ -47,7 +48,7 @@ class DatasetMultimodal(Dataset):
             items_paths = df_filtered['file_path'].tolist()
             idx=idx+1
 
-        items_paths=items_paths[:8092]
+        items_paths=items_paths[:8192]
 
         n_tokens=0
         all_tokens_tensor = torch.empty(0)
@@ -58,11 +59,11 @@ class DatasetMultimodal(Dataset):
             if df_filtered['type'][i]=="ts" :
 
                 print(i, "one ts timestep used")
-                for n_column in range(1,9):
-                    one_value = df_filtered.iloc[0, n_column].astype(float) #dd
+                for n_column in range(1,9): #on parcourt chaque variable indépendamment
+                    one_value = df_filtered.iloc[0, n_column].astype(float) 
                     context = torch.tensor( one_value )
-                    context = context.reshape(1,1)
-                    new_token, _ = self.pipeline_ts.embed(context) # il faudra un positionnal encoding geographique par station + taille petite et pas 512.
+                    context = context.reshape(1,1) 
+                    new_token, _ = self.pipeline_ts.embed(context) # il faudra un positionnal encoding geographique par station, VARIABLE + taille petite et pas 512.
                     new_token=new_token[:,1,:].to("cuda")
                     new_token= new_token.unsqueeze(dim=2)
                     all_tokens_tensor = torch.cat((all_tokens_tensor, new_token), dim=2)
@@ -85,42 +86,50 @@ class DatasetMultimodal(Dataset):
                 quant_t_flat = torch.flatten(quant_t, start_dim=2)
                 quant_b_flat = torch.flatten(quant_b, start_dim=2)
                 new_tokens = torch.cat((quant_t_flat, quant_b_flat), dim=2)
-                #this is ugly. pads from size 64 to 512 
-                zeros_to_add = torch.zeros(1, 512 - 64, new_tokens.size(2)).to("cuda")
+                zeros_to_add = torch.zeros(1, 512 - 64, new_tokens.size(2)).to("cuda") #this is ugly. pads from size 64 to 512
                 new_tokens = torch.cat((new_tokens, zeros_to_add), dim=1)
 
                 all_tokens_tensor = torch.cat((all_tokens_tensor, new_tokens), dim=2)
                 n_tokens=n_tokens+320
 
-            if all_tokens_tensor.size(2)>8092:
+            if all_tokens_tensor.size(2)>8192:
                 break
         
-        if all_tokens_tensor.size(2)>8092:
-            all_tokens_tensor=all_tokens_tensor[:,:,:8092] 
+        if all_tokens_tensor.size(2)>8192:
+            all_tokens_tensor=all_tokens_tensor[:,:,:8192] 
+            n_tokens = 8192
         return  all_tokens_tensor, n_tokens
    
-"""
-def collate_fn(data): # masquage aleatoire, extraction du contenu caché, pad a la taille max
-    
-    sequence, mask_sequence = zip(*data)
-    #faire le dynamic padding et renvoyer batch_masqué, contenu des trous
-    tokens_concat, lengths = zip(*data)
 
-    #les masquer aléatoirement et sortir le contenu des trous
-    #padding + batch
+def my_collate_fn(batch):
+    print("collat_fn is called")
+    # Create lists to hold the masked sequences and the original contents of the masked positions
+    masked_batch = []
+    batch_of_labels = []
 
-    max_len = max(lengths)
-    n_ftrs = data[0][0].size(1)
-    features = torch.zeros((len(data), max_len, n_ftrs))
-    labels = torch.tensor(labels)
-    lengths = torch.tensor(lengths)
+    print("aaaaaaaaaaa", len(batch) )
+    print("bbbbbbbbbbbbbbb", type(batch[0]))
 
-    for i in range(len(data)):
-        j, k = data[i][0].size(0), data[i][0].size(1)
-        features[i] = torch.cat([data[i][0], torch.zeros((max_len - j, k))])
+    for sequence in batch[0]:
+        sequence_length = len(sequence)
+        mask_indices = random.sample(range(sequence_length), k=int(0.2 * sequence_length))
+        
+        masked_sequence = sequence.clone()
+        labels = torch.zeros_like(sequence)
 
-    return batch_masqué, batch_des_labels
-"""
+        for idx in mask_indices:
+            labels[idx] = sequence[idx]
+            masked_sequence[idx] = 0  # Assuming 0 is the masking value
+
+        masked_batch.append(masked_sequence)
+        batch_of_labels.append(labels)
+
+    # Pad sequences to the max length
+    masked_batch = pad_sequence(masked_batch, batch_first=True, padding_value=0)
+    batch_of_labels = pad_sequence(batch_of_labels, batch_first=True, padding_value=0)
+
+    return masked_batch, batch_of_labels
+
 
 if __name__ == "__main__":
 
@@ -146,20 +155,24 @@ if __name__ == "__main__":
     absolute_start = "2016-01-01 00:00:00"
     absolute_end =  "2019-01-01 00:00:00"
     
-    dataset_images = DatasetMultimodal(df_pandas, absolute_start, absolute_end, transforms, images_tokenizer, pipeline_ts)
-    print("Longueur totale du dataset:", len(dataset_images))
-    
-    seq_tokens, n = dataset_images[0]
-    print("taille seq tokens", seq_tokens.size() )
+    dataset_multi = DatasetMultimodal(df_pandas, absolute_start, absolute_end, transforms, images_tokenizer, pipeline_ts)
+    print("Longueur totale du dataset multimodal(nombre de séquences différentes):", len(dataset_multi))
+           
+           
+    #contrôle sur une sequence
+    """print("génère une seuqnce")
+    seq_tokens, n = dataset_multi[0]
+    print("taille de le premiere sequence", seq_tokens.size() )"""
 
-    dataloader_images = DataLoader(dataset_images, shuffle=True, batch_size=10 )
-    print("Taille des lots:", dataloader_images.batch_size)
-    for batch_idx, out in enumerate(dataloader_images):
+    dataloader_multi = DataLoader(dataset_multi, shuffle=True, batch_size=1, collate_fn = my_collate_fn)
+    print("Taille des lots:", dataloader_multi.batch_size)
+
+    for batch_idx, out in enumerate(dataloader_multi):
         print("Batch", batch_idx)
-        print("tokens:", out[0].size())  
+        print("contenu du batch, tokens", out[0].size())  
+        print("contenu du batch, partie n_tokens:", out[1]) 
 
-        if batch_idx >= 1:
-            break
+        break
 
         
     """#check all by recontructing one image
@@ -173,5 +186,3 @@ if __name__ == "__main__":
     output = images_tokenizer.decode( quant_t, quant_b )
     utils.save_image( output, "verif_reconstruction.png", normalize=True, range=(-1,1))
     print("fin")"""
-
-
